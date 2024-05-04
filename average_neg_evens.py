6a3556117e300c87c4750a6bfcd0dfcab7910bcb
Chris Courville <ccourville80@gmail.com>
2024-05-04 12:36:07 -0400

    Succesful operation ready for sumission

--- average_neg_evens.py
+++ average_neg_evens.py
@@ -3,20 +3,23 @@
 #Date:  5/3/24
 
 # list = [0,1,2,-2,-3,-4,3,4]
-import statistics
-from statistics import mean
-
 def average_neg_evens(list):
     sum_n_e = 0
+    cnt_n_e = 0
+     
     for i in list:
-        if i < 0 and i % 2 == 0:
+        if i < 0 and (i % 2) == 0:
             sum_n_e += i
-        else:   #negative and even
+            cnt_n_e += 1
 
-    avg_n_e = sum_n_e.mean()
-        
-    return sum_n_e
+    avg_n_e = sum_n_e / cnt_n_e
+         
+    return avg_n_e
+
+
 def main():
-    #test count_vowels
+    #test avg neg even
     print(average_neg_evens([0,1,2,-2,-3,-4,3,4])) # working function call
     print('average_neg_evens', average_neg_evens([0,1,2,-2,-3,-4,3,4]) == -3)
+
+main()
