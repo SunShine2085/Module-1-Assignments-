8ec2482ca9e8cc3ece7d1dff7ada4715bb2c0245
Chris Courville <136990672+SunShine2085@users.noreply.github.com>
2024-05-06 17:38:28 -0400

    data_project_practice

---
+++ data_project_practice.py
@@ -0,0 +1,34 @@
+#Name:  Chris Courville
+#Date:  5/6/24
+#Proj:  Data Project Practice
+
+# Write a program that reads in the csv file linked above and outputs the mean, median, 
+# and standard deviation for the fall & spring semesters. Make sure to write at least
+# two functions in your final solution. The more generalizable you make your code, 
+# the more you may be able to reuse it for your own project later.
+import statistics
+#Read in file
+def output_stats():
+    print("Mean: ", statistics.mean(list))
+    print("Median: ", statistics.median(list))
+    print("STD: ", statistics.stdev(list))
+
+
+spring = []
+fall = []
+
+csv = "sample_grades.csv"
+file = open(csv)
+for line in file:
+    list = line.rsrtip().split(",")
+    if list[1] == 'Spring 2016':
+        spring.append(eval(list[2]))
+    else:
+        fall.append(eval(list[2]))
+file.close()
+
+print("Fall 2016:")
+output_stats(fall)
+
+print("Spring 2016")
+output_stats(spring)
