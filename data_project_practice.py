4fe165e25238c3a71f500856053c5be7db2ed095
Chris Courville <ccourville80@gmail.com>
2024-05-06 19:42:34 -0400

    Ready for submission

--- data_project_practice.py
+++ data_project_practice.py
@@ -8,27 +8,33 @@
 # the more you may be able to reuse it for your own project later.
 import statistics
 #Read in file
-def output_stats():
+def output_stats(list):
     print("Mean: ", statistics.mean(list))
     print("Median: ", statistics.median(list))
     print("STD: ", statistics.stdev(list))
 
 
+def line_count():
+    for line in file:
+        list = line.rstrip().split(",")
+        if list[1] == 'Spring 2016':
+            spring.append(eval(list[2]))
+        else:
+            fall.append(eval(list[2]))
+#Data Variables
 spring = []
 fall = []
 
 csv = "sample_grades.csv"
 file = open(csv)
 for line in file:
-    list = line.rsrtip().split(",")
-    if list[1] == 'Spring 2016':
-        spring.append(eval(list[2]))
-    else:
-        fall.append(eval(list[2]))
+    line_count()
 file.close()
+
+print(list)
 
 print("Fall 2016:")
 output_stats(fall)
 
-print("Spring 2016")
+print("Spring 2016:")
 output_stats(spring)
